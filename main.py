4ab2771da1f78889eaa575577f6002cbf9a94ba2
streamlit-alex <118288805+streamlit-alex@users.noreply.github.com>
2022-11-15 15:34:14 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import time  # to simulate a real time data, time loop
-
-import numpy as np  # np mean, np random
 import pandas as pd  # read csv, df manipulation
-import plotly.express as px  # interactive charts
 import streamlit as st  # 🎈 data web app development
 
 st.set_page_config(
